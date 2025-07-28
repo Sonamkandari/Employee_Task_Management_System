f60bb5c7c8b55b325e9f61ae3925ec63b9d8ab27
sonam kandari <sonamkandari2004@gmail.com>
2025-07-28 14:52:50 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Employee_Task_Management_System/',
   plugins: [react(), tailwindcss(),],
 })
